b74b5007f9307c22aeacbf129bc099a5eb8dc7e7
Eun Oh <bdohhhhh@gmail.com>
2024-05-24 19:12:57 +0900

    240524 [add] : add annotation

--- assignments/04-memo/src/hooks/useMemoApp.tsx
+++ assignments/04-memo/src/hooks/useMemoApp.tsx
@@ -2,11 +2,13 @@ import { AppDispatch, RootState } from "@/redux/store";
 import { CREATE, DELETE, SELECT, UPDATE } from "@/types/d";
 import { useDispatch, useSelector } from "react-redux";
 
+// 얘는 커스텀 훅이고
 function useMemoApp() {
     const dispatch = useDispatch<AppDispatch>();
     const memos = useSelector((state: RootState) => state.memoApp.memos);
     const selected = useSelector((state: RootState) => state.memoApp.selected);
 
+    // 얘네들이 액션크리에이터임!
     const selectMemo = (memoId: string) =>
         dispatch({ type: SELECT, payload: memoId });
     const createMemo = (text: string) =>
